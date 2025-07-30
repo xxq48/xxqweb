6ab7d1bb6474468d0a03f6c0915cdacb2336fd20
xxq <3393641473@qq.com>
2025-07-30 11:38:00 +0800

    feat: 更新菜单项和表格数据结构，添加用户管理和销售管理功能

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,20 @@
   <el-container class="layout-container-demo" style="height: 100vh">
     <el-aside width="200px">
       <el-scrollbar>
-        <el-menu :default-openeds="['1', '3']">
-          <el-sub-menu index="1">
+        <el-menu :default-openeds="['1', '3']">    
+          <!-- 整个菜单 -->
+          <el-sub-menu index="1">     
+            <!-- 子菜单 -->
             <template #title>
-              <el-icon><message /></el-icon>Navigator One
+              <el-icon><message /></el-icon>销售模块   
+              <!-- 子菜单标题 -->
             </template>
             <el-menu-item-group>
-              <template #title>Group 1</template>
-              <el-menu-item index="1-1">Option 1</el-menu-item>
-              <el-menu-item index="1-2">Option 2</el-menu-item>
+              <template #title>Group 1</template>      
+              <!-- 子菜单组标题 -->
+              <el-menu-item index="1-1"><el-icon><Avatar /></el-icon>用户管理</el-menu-item>    
+              <!-- 子菜单项 -->
+              <el-menu-item index="1-2">销售管理</el-menu-item>
             </el-menu-item-group>
             <el-menu-item-group title="Group 2">
               <el-menu-item index="1-3">Option 3</el-menu-item>
@@ -79,10 +84,12 @@
 
       <el-main>
         <el-scrollbar>
-          <el-table :data="tableData">
-            <el-table-column prop="date" label="Date" width="140" />
-            <el-table-column prop="name" label="Name" width="120" />
-            <el-table-column prop="address" label="Address" />
+          <el-table :data="state.tableData">
+            <el-table-column prop="suId" label="编号" width="140" />
+            <el-table-column prop="suName" label="名称" width="120" />
+            <el-table-column prop="suPwd" label="密码" width="120"/>
+            <el-table-column prop="suRole" label="角色" width="120" />
+            <el-table-column prop="suTime" label="时间" width="120" />
           </el-table>
         </el-scrollbar>
       </el-main>
@@ -91,15 +98,29 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
-import { Menu as IconMenu, Message, Setting } from '@element-plus/icons-vue'
+import { Menu as IconMenu, Message, Setting,Avatar } from "@element-plus/icons-vue";
+import { reactive, onMounted } from "vue";
+import axios from "axios";
 
-const item = {
-  date: '2016-05-02',
-  name: 'Tom',
-  address: 'No. 189, Grove St, Los Angeles',
-}
-const tableData = ref(Array.from({ length: 20 }).fill(item))
+// 定义页面的数据变量对象state
+const state = reactive({
+  tableData: [],
+});
+
+// 获取后台数据的方法定义
+const getData = () => {
+  axios({
+    method: "get",
+    url: "http://localhost:8080/sysUser/listUser"
+  }).then((res) => {
+    state.tableData = res.data;
+  });
+};
+
+onMounted(() => {
+  // 初始化表格数据
+  getData();
+});
 </script>
 
 <style scoped>
@@ -108,7 +129,7 @@ const tableData = ref(Array.from({ length: 20 }).fill(item))
   background-color: var(--el-color-primary-light-7);
   color: var(--el-text-color-primary);
 }
-.layout-container-demo .el-aside {
+.layout-container-demo .el-aside.el-menu.el-sub-menu.el-menu-item.el-menu-item-group {
   color: var(--el-text-color-primary);
   background: var(--el-color-primary-light-8);
 }
@@ -125,4 +146,7 @@ const tableData = ref(Array.from({ length: 20 }).fill(item))
   height: 100%;
   right: 20px;
 }
+
+
+
 </style>
